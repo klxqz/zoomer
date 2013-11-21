3f2e47829c139cc7636de3e34d36eeca158adda2
klxqz <klxqz@mail.ru>
2013-11-21 11:59:41 +0600

    релиз

--- lib/actions/shopZoomerPluginBackendSave.controller.php
+++ lib/actions/shopZoomerPluginBackendSave.controller.php
@@ -1,5 +1,9 @@
 <?php
 
+/**
+ * @author Коробонв Николай wa-plugins.ru <support@wa-plugins.ru>
+ * @link http://wa-plugins.ru/
+ */
 class shopZoomerPluginBackendSaveController extends waJsonController {
 
     protected $tmp_path = 'plugins/zoomer/templates/FrontendProduct.html';
