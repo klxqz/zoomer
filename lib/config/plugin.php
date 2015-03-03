d7ceaf617ac7e1cb0a6d04edf81dbf59fe3483a7
admin <admin@xstream>
2015-03-03 11:36:06 +0600

    исправление неверного форматирования кода

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -8,7 +8,7 @@ return array(
     'name' => 'Лупа',
     'description' => 'Эффект лупы при увеличение изображения',
     'vendor' => '985310',
-    'version' => '2.0.1',
+    'version' => '2.0.2',
     'img' => 'img/zoomer.png',
     'shop_settings' => true,
     'frontend' => true,
